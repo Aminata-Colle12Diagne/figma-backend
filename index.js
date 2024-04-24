f8c8f63804786362a3d10d59f1b6710f0f630f46
Aminata-Colle12Diagne <diagneaminata464@gmail.com>
2024-04-24 12:14:16 +0000

    modification  basic express

--- index.js
+++ index.js
@@ -27,60 +27,69 @@ require('./db/connection');
 const Users = require('./Model/User');
 const UserModal = require('./Model/Data');
 
-app.post("/", async (req, res) => {
-    let user = new Users(req.body);
-    let result = await user.save();
-    res.send(result);
-});
+app.get('/', (req, res) => res.send('Hello world'));
 
-app.get("/getHotels", async (req, res) => {
-    UserModal.find({})
-        .then(function (ut) {
-            res.json(ut);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-});
 
-app.post('/forgot-password', (req, res) => {
-    crypto.randomBytes(32, (err, Buffer) => {
-        if (err) {
-            console.log(err);
-        }
-        const token = Buffer.toString("hex");
-        Users.findOne({ email: req.body.email })
-            .then(user => {
-                if (!user) {
-                    return res.status(422).json({ error: "User does not exist with that email" });
-                }
-                user.resetToken = token;
-                user.save().then(result => {
-                    Transporter.sendMail({
-                        to: user.email,
-                        from: "no-replay@insta.com",
-                        subject: "password reset",
-                        html: `
-                            <p>You requested for password reset</p>
-                            <h5>click in this <a href="https://figma-backend-mocha.vercel.app/forgot-password/${token}" >link</a>to reset password</h5>`
-                    });
-                });
-            });
-    });
-});
 
-app.post("/addHotel", async (req, res) => {
-    const hotelData = req.body;
+// app.post("/", async (req, res) => {
+//     let user = new Users(req.body);
+//     let result = await user.save();
+//     res.send(result);
+// });
 
-    try {
-        const newHotel = new UserModal(hotelData);
-        await newHotel.save();
-        res.json(newHotel);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Erreur lors de l'ajout des données de l'hôtel");
-    }
-});
+// app.get("/getHotels", async (req, res) => {
+//     try {
+//         UserModal.find({})
+//         .then(function (ut) {
+//             res.json(ut);
+//         })
+//         .catch(function (err) {
+//             res.json(err);
+//         });
+//     } catch (error) {
+//         console.log('erreur')
+//     }
+   
+// });
+
+// app.post('/forgot-password', (req, res) => {
+//     crypto.randomBytes(32, (err, Buffer) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         const token = Buffer.toString("hex");
+//         Users.findOne({ email: req.body.email })
+//             .then(user => {
+//                 if (!user) {
+//                     return res.status(422).json({ error: "User does not exist with that email" });
+//                 }
+//                 user.resetToken = token;
+//                 user.save().then(result => {
+//                     Transporter.sendMail({
+//                         to: user.email,
+//                         from: "no-replay@insta.com",
+//                         subject: "password reset",
+//                         html: `
+//                             <p>You requested for password reset</p>
+//                             <h5>click in this <a href="https://figma-backend-mocha.vercel.app/forgot-password/${token}" >link</a>to reset password</h5>`
+//                     });
+//                 });
+//             });
+//     });
+// });
+
+// app.post("/addHotel", async (req, res) => {
+//     const hotelData = req.body;
+
+//     try {
+//         const newHotel = new UserModal(hotelData);
+//         await newHotel.save();
+//         res.json(newHotel);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send("Erreur lors de l'ajout des données de l'hôtel");
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`Serveur démarré sur http://0.0.0.0:${PORT}`);
