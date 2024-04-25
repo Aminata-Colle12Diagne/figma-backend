bc5afdb5d451af1ce33b811a379c2ae7e4bfcb15
Aminata-Colle12Diagne <diagneaminata464@gmail.com>
2024-04-25 12:24:23 +0000

    modification de connexion

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://collediagne98:colle98@cluster0.r5owzhb.mongodb.net/authentification?retryWrites=true&w=majority&appName=Cluster0',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect('mongodb+srv://collediagne98:colle98@cluster0.r5owzhb.mongodb.net/authentification?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
+
+
